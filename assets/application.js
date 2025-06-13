bcd0850e0cfb79a86c43eb8f1c7ad5e20bda34e3
Ramrishkumar <155762886+Ramrishkumar@users.noreply.github.com>
2025-06-13 11:51:19 +0530

    Update application.js

--- assets/application.js
+++ assets/application.js
@@ -4,7 +4,7 @@
 // ================================ header section ================================
 document.addEventListener("DOMContentLoaded", function () {
   const header = document.querySelector('.site-header');
-
+  
   window.addEventListener('scroll', function () {
     if (window.scrollY > 50) {
       header.classList.add('scrolled');
